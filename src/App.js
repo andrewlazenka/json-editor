dc7edd2c8b95d19509b19421fe01beb84c97f76c
Andrew Lazenka <andrewlazenka@gmail.com>
2020-08-31 21:23:07 -0400

    Update deps, style app options

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import styles from "./App.module.css";
 import Data from "./data";
 import JSONParser from "./JSONParser";
 
@@ -13,41 +15,53 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to JSON Parser</h1>
-        <label htmlFor="colorize-arrays">Highlight Array Items</label>
-        <input
-          type="checkbox"
-          id="colorize-arrays"
-          checked={colorizeArrays}
-          onChange={() => setColorizeArrays(!colorizeArrays)}
-        />
-        <label htmlFor="colorize-objects">Highlight Objects</label>
-        <input
-          type="checkbox"
-          id="colorize-objects"
-          checked={highlightObjects}
-          onChange={() => setHighlightObjects(!highlightObjects)}
-        />
-        <label htmlFor="outline-objects">Outline Objects on Hover</label>
-        <input
-          type="checkbox"
-          id="outline-objects"
-          checked={outlineObjects}
-          onChange={() => setOutlineObjects(!outlineObjects)}
-        />
-        <label htmlFor="outline-arrays">Outline Arrays on Hover</label>
-        <input
-          type="checkbox"
-          id="outline-arrays"
-          checked={outlineArrays}
-          onChange={() => setOutlineArrays(!outlineArrays)}
-        />
-        <label htmlFor="outline-fields">Outline Fields on Hover</label>
-        <input
-          type="checkbox"
-          id="outline-fields"
-          checked={outlineFields}
-          onChange={() => setOutlineFields(!outlineFields)}
-        />
+        <div className={styles.appOptions}>
+          <span>
+            <label htmlFor="colorize-arrays">Highlight Array Items</label>
+            <input
+              type="checkbox"
+              id="colorize-arrays"
+              checked={colorizeArrays}
+              onChange={() => setColorizeArrays(!colorizeArrays)}
+            />
+          </span>
+          <span>
+            <label htmlFor="colorize-objects">Highlight Objects</label>
+            <input
+              type="checkbox"
+              id="colorize-objects"
+              checked={highlightObjects}
+              onChange={() => setHighlightObjects(!highlightObjects)}
+            />
+          </span>
+          <span>
+            <label htmlFor="outline-objects">Outline Objects on Hover</label>
+            <input
+              type="checkbox"
+              id="outline-objects"
+              checked={outlineObjects}
+              onChange={() => setOutlineObjects(!outlineObjects)}
+            />
+          </span>
+          <span>
+            <label htmlFor="outline-arrays">Outline Arrays on Hover</label>
+            <input
+              type="checkbox"
+              id="outline-arrays"
+              checked={outlineArrays}
+              onChange={() => setOutlineArrays(!outlineArrays)}
+            />
+          </span>
+          <span>
+            <label htmlFor="outline-fields">Outline Fields on Hover</label>
+            <input
+              type="checkbox"
+              id="outline-fields"
+              checked={outlineFields}
+              onChange={() => setOutlineFields(!outlineFields)}
+            />
+          </span>
+        </div>
         <JSONParser
           collapsable={false}
           highlightObjects={highlightObjects}
